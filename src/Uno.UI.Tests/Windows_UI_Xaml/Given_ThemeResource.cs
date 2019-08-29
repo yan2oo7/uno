534c10a7d2fed2f160eb0cf880b3778e0f2b3a05
David Oliver <david.oliver@nventive.com>
2019-08-29 11:30:44 -0400

    Add theme resource resolution tests

--- src/Uno.UI.Tests/Windows_UI_Xaml/Given_ThemeResource.cs
+++ src/Uno.UI.Tests/Windows_UI_Xaml/Given_ThemeResource.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Uno.UI.Tests.App.Xaml;
 using Windows.UI;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Media;
 
 namespace Uno.UI.Tests.Windows_UI_Xaml
@@ -22,6 +23,29 @@ namespace Uno.UI.Tests.Windows_UI_Xaml
 			app.HostView.Children.Add(control);
 
 			Assert.AreEqual(Color.FromArgb(0xDE, 0x00, 0x00, 0x00), (control.TestTextBlock.Foreground as SolidColorBrush).Color);
+		}
+
+		[TestMethod]
+		public void When_App_ThemeResource_Default()
+		{
+			var app = UnitTestsApp.App.EnsureApplication();
+
+			var control = new Test_Control();
+			app.HostView.Children.Add(control);
+
+			Assert.AreEqual(Colors.LemonChiffon, (control.TestBorder.Background as SolidColorBrush).Color);
+		}
+
+		[TestMethod]
+		public void When_App_ThemeResource_Light()
+		{
+			var app = UnitTestsApp.App.EnsureApplication();
+
+			var control = new Test_Control();
+			app.HostView.Children.Add(control);
+
+			Assert.AreEqual(ApplicationTheme.Light, app.RequestedTheme);
+			Assert.AreEqual(Colors.RosyBrown, (control.TestBorder.BorderBrush as SolidColorBrush).Color);
 		}
 	}
 }
