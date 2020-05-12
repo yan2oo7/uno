9097e66040688f2481a69643a1296b70e3c83ac1
David Oliver <david.oliver@nventive.com>
2020-05-12 13:06:38 -0400

    feat(progressring)!: [Android] [iOS] Make WUX.ProgressRing inherit from Control
    BREAKING CHANGE: ProgressRing on Android/iOS now inherits from Control. It's still using the native control to display the progress ring, which is now nested inside the WinUI one.

--- src/Uno.UI/Generated/3.0.0.0/Windows.UI.Xaml.Controls/ProgressRing.cs
+++ src/Uno.UI/Generated/3.0.0.0/Windows.UI.Xaml.Controls/ProgressRing.cs
@@ -7,7 +7,7 @@ namespace Windows.UI.Xaml.Controls
 	#endif
 	public  partial class ProgressRing 
 	{
-		#if false || false || NET461 || false || false
+#if false || false || NET461 || false || false
 		[global::Uno.NotImplemented]
 		public  bool IsActive
 		{
@@ -20,8 +20,8 @@ namespace Windows.UI.Xaml.Controls
 				this.SetValue(IsActiveProperty, value);
 			}
 		}
-		#endif
-		#if __ANDROID__ || __IOS__ || NET461 || false || __MACOS__
+#endif
+#if false || false || false || false || false
 		[global::Uno.NotImplemented]
 		public  global::Windows.UI.Xaml.Controls.Primitives.ProgressRingTemplateSettings TemplateSettings
 		{
@@ -30,22 +30,22 @@ namespace Windows.UI.Xaml.Controls
 				throw new global::System.NotImplementedException("The member ProgressRingTemplateSettings ProgressRing.TemplateSettings is not implemented in Uno.");
 			}
 		}
-		#endif
-		#if false || false || NET461 || false || false
+#endif
+#if false || false || NET461 || false || false
 		[global::Uno.NotImplemented]
 		public static global::Windows.UI.Xaml.DependencyProperty IsActiveProperty { get; } = 
 		Windows.UI.Xaml.DependencyProperty.Register(
 			nameof(IsActive), typeof(bool), 
 			typeof(global::Windows.UI.Xaml.Controls.ProgressRing), 
 			new FrameworkPropertyMetadata(default(bool)));
-		#endif
-		#if false || false || NET461 || false || false
+#endif
+#if false || false || NET461 || false || false
 		[global::Uno.NotImplemented]
 		public ProgressRing() : base()
 		{
 			global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.UI.Xaml.Controls.ProgressRing", "ProgressRing.ProgressRing()");
 		}
-		#endif
+#endif
 		// Forced skipping of method Windows.UI.Xaml.Controls.ProgressRing.ProgressRing()
 		// Forced skipping of method Windows.UI.Xaml.Controls.ProgressRing.IsActive.get
 		// Forced skipping of method Windows.UI.Xaml.Controls.ProgressRing.IsActive.set
